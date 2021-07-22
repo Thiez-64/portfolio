b4ed6d36f6c9521050726ce6c4cb496b5e7e98ae
Thiez-64 <ma.ojeanson@gmail.com>
2021-07-22 22:51:54 +0200

    Delete a point virgule

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,7 +14,7 @@ function MyApp({ Component, pageProps }) {
     <RecoilRoot>
       <ThemeProvider>
         <Layout>
-          <Component {...pageProps} />;
+          <Component {...pageProps} />
         </Layout>
       </ThemeProvider>
     </RecoilRoot>
