42abb271c8002b247afd0e4620b04489f2d4f528
Thiez-64 <ma.ojeanson@gmail.com>
2021-07-22 12:25:47 +0200

    CRUD stacks have been done

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -4,12 +4,16 @@ import DarkModeToggle from "./darkmodetoggle";
 
 export default function NavBar(): JSX.Element {
   return (
-    <div>
-      <Image src={Portrait} alt="portrait" width={174} height={261} />
+    <div className="flex items-center w-full justify-around">
+      <div className="w-14">
+        <Image src={Portrait} alt="portrait" width={174 / 2} height={261 / 2} />
+      </div>
       <h1 className="dark:text-red-500">
         Matthieu O’Jeanson, Web and Mobile Applications Developer
       </h1>
-      <DarkModeToggle />
+      <div className="w-14">
+        <DarkModeToggle />
+      </div>
     </div>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,79 @@
-export default function Home() {
+import { Stack } from "@prisma/client";
+import { GetStaticProps, InferGetStaticPropsType } from "next";
+import Image from "next/image";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+type InputStack = {
+  stack: string;
+  logo: string;
+};
+
+export default function Home({
+  stacks,
+}: InferGetStaticPropsType<typeof getStaticProps>): JSX.Element {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<InputStack>();
+  const onSubmit: SubmitHandler<InputStack> = async (data) => {
+    console.log(data);
+    await fetch("http://localhost:3000/api/stacks", {
+      method: "POST", // or 'PUT'
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(data),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Success:", data);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      });
+  };
+  console.log(stacks);
   return (
     <div>
-      <main></main>
-      <footer></footer>
+      <h2>Web Skills</h2>
+      <ul>
+        {stacks.map((stack, index) => {
+          return (
+            <li key={stack.id}>
+              <Image src={stack.logo} alt="logo" width={50} height={50} />
+              <p>{stack.stack}</p>
+            </li>
+          );
+        })}
+      </ul>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        {/* register your input into the hook by invoking the "register" function */}
+        <input
+          defaultValue="Stack ?"
+          {...register("stack", { required: true })}
+        />
+
+        {/* include validation with required or other standard HTML validation rules */}
+        <input defaultValue="Logo ?" {...register("logo")} />
+        {/* errors will return when field validation fails  */}
+        {errors.stack && <span>This field is required</span>}
+
+        <input type="submit" />
+      </form>
     </div>
   );
 }
+
+export const getStaticProps: GetStaticProps<{ stacks: Stack[] }> = async (
+  context
+) => {
+  const stacks = await fetch("http://localhost:3000/api/stacks").then((res) =>
+    res.json()
+  );
+
+  return {
+    props: { stacks },
+  };
+};
